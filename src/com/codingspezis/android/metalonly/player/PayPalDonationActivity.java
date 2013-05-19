dd5dfd9d443089eeb30074cea9b0dcd90a97a6ae
Jan Lippert <lippertsjan@gmail.com>
2013-05-19 03:28:33 +0200

    Added up navigation and currency in hint. currency is missing when a donation value is set though

--- src/com/codingspezis/android/metalonly/player/PayPalDonationActivity.java
+++ src/com/codingspezis/android/metalonly/player/PayPalDonationActivity.java
@@ -1,8 +1,11 @@
 package com.codingspezis.android.metalonly.player;
 
+import android.content.*;
 import android.os.*;
+import android.support.v4.app.*;
 
 import com.actionbarsherlock.app.*;
+import com.actionbarsherlock.view.*;
 
 /**
  * 
@@ -14,7 +17,27 @@ public class PayPalDonationActivity extends SherlockFragmentActivity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		getSupportActionBar().setHomeButtonEnabled(true);
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 		setContentView(R.layout.acticity_donation);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			navigateUp();
+			return true;
+
+		default:
+			return super.onOptionsItemSelected(item);
+
+		}
+	}
+
+	private void navigateUp() {
+		Intent upIntent = new Intent(this, MainActivity.class);
+		NavUtils.navigateUpTo(this, upIntent);
 	}
 
 }
