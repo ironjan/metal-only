dca82d64a141e943ba3113532af16d1b41d5d5f3
Jan Lippert <lippertsjan@gmail.com>
2013-11-04 21:51:11 +0100

    Fixed #38, had to increase min api level

--- src/com/codingspezis/android/metalonly/player/stream/PlayerBCReceiver.java
+++ src/com/codingspezis/android/metalonly/player/stream/PlayerBCReceiver.java
@@ -16,7 +16,9 @@ public class PlayerBCReceiver extends BroadcastReceiver {
 	 */
 	private final PlayerService playerService;
 
-	private AudioManager audioManager;
+	AudioManager audioManager;
+
+	private OnAudioFocusChangeListener afChangeListener;
 
 	/**
 	 * @param playerService
@@ -25,39 +27,64 @@ public class PlayerBCReceiver extends BroadcastReceiver {
 		this.playerService = playerService;
 		audioManager = (AudioManager) playerService
 				.getSystemService(Context.AUDIO_SERVICE);
-		
+
+		afChangeListener = new OnAudioFocusChangeListener() {
+
+			@Override
+			public void onAudioFocusChange(int focusChange) {
+				if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT) {
+					stop();
+				}
+				else if (focusChange == AudioManager.AUDIOFOCUS_GAIN) {
+					play();
+				}
+				else if (focusChange == AudioManager.AUDIOFOCUS_LOSS) {
+					audioManager.abandonAudioFocus(this);
+					stop();
+				}
+			}
+		};
 	}
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
-		if (intent.getAction().equals(PlayerService.INTENT_PLAY)) {
+		String action = intent.getAction();
+		if (action.equals(PlayerService.INTENT_PLAY)) {
 			play();
-		}
-		else if (intent.getAction().equals(PlayerService.INTENT_STOP)) {
-			stop();
-		}
-		else if (intent.getAction().equals(PlayerService.INTENT_STATUS_REQUEST)) {
 			sendPlayerStatus();
 		}
-		else if (intent.getAction().equals(PlayerService.INTENT_EXIT)) {
+		else if (action.equals(PlayerService.INTENT_STOP)) {
+			stop();
+			sendPlayerStatus();
+		}
+		else if (action.equals(PlayerService.INTENT_STATUS_REQUEST)) {
+			sendPlayerStatus();
+		}
+		else if (action.equals(PlayerService.INTENT_EXIT)) {
 			exit();
 		}
 	}
 
-	private void play() {
-		if (playerService.audioStream != null) {
-			playerService.audioStream.stopPlaying();
+	void play() {
+		stop();
+
+		int result = audioManager.requestAudioFocus(afChangeListener,
+				AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN);
+
+		if (AudioManager.AUDIOFOCUS_REQUEST_GRANTED == result) {
+			playerService.instantiateSelectedPlayer();
+			playerService.streamPlaying = true;
+			playerService.audioStream
+					.setOnStreamListener(playerService.streamWatcher);
+
+			playerService.audioStream.startPlaying();
 		}
-
-		playerService.instantiateSelectedPlayer();
-		playerService.streamPlaying = true;
-		playerService.audioStream
-				.setOnStreamListener(playerService.streamWatcher);
-
-		playerService.audioStream.startPlaying();
+		else {
+			stop();
+		}
 	}
 
-	private void stop() {
+	void stop() {
 		if (playerService.audioStream != null) {
 			playerService.audioStream.stopPlaying();
 		}
