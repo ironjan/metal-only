fcafcefed85e6248d958effe0d58a6df2b9261a2
Jan Lippert <lippertsjan@gmail.com>
2014-03-18 21:03:03 +0100

    Added travis integration and merged conflicts

--- src/main/java/com/codingspezis/android/metalonly/player/wish/WishChecker.java
+++ src/main/java/com/codingspezis/android/metalonly/player/wish/WishChecker.java
@@ -1,97 +1,99 @@
 package com.codingspezis.android.metalonly.player.wish;
 
+import java.io.*;
+
 import android.content.*;
 
 import com.codingspezis.android.metalonly.player.siteparser.*;
 
-import java.io.*;
-
 /**
+ * 
  * checks for open wishes / regards on wish page on metal-only.de
- * TODO refactor this class
+ * 
  */
 public class WishChecker {
 
-    private OnWishesCheckedListener wishListener;
+	private OnWishesCheckedListener wishListener;
 
-    private final HTTPGrabber grabber;
-    private final OnHTTPGrabberListener grabberListener =
+	private final HTTPGrabber grabber;
+	private final OnHTTPGrabberListener grabberListener =
 
-            new OnHTTPGrabberListener() {
+	new OnHTTPGrabberListener() {
 
-                @Override
-                public void onSuccess(BufferedReader httpResponse) {
-                    AllowedActions allowedActions = new AllowedActions();
-                    allowedActions.regards = true;
-                    allowedActions.wishes = true;
-                    allowedActions.moderated = true;
-                    try {
-                        String line = httpResponse.readLine();
-                        while (line != null) {
-                            // format:
-                            // "Derzeitiges Limit: 4 Wünsche und 3 Grüße pro Hörer"
-                            if (line.contains("Derzeitiges Limit:")) {
-                                try {
-                                    allowedActions.limit = line.substring(
-                                            line.indexOf("Derzeitiges Limit:"),
-                                            line.indexOf("pro Hörer")).trim();
-                                } catch (Exception e) {
-                                    e.printStackTrace();
-                                }
-                            }
-                            if (line.contains("sind keine Grüße möglich.")) {
-                                allowedActions.regards = false;
-                            }
-                            if (line.contains("die Playlist ist bereits voll")) {
-                                allowedActions.wishes = false;
-                            }
-                            if (line.contains("sind nur in der moderierten Sendezeit möglich!")
-                                    || line.contains("Aktuell On Air: MetalHead")) {
-                                allowedActions.regards = false;
-                                allowedActions.wishes = false;
-                                allowedActions.moderated = false;
-                                break;
-                            }
-                            line = httpResponse.readLine();
-                        }
-                        if (wishListener != null) {
-                            wishListener.onWishesChecked(allowedActions);
-                        }
+		@Override
+		public void onSuccess(BufferedReader httpResponse) {
+			AllowedActions allowedActions = new AllowedActions();
+			allowedActions.regards = true;
+			allowedActions.wishes = true;
+			allowedActions.moderated = true;
+			try {
+				String line = httpResponse.readLine();
+				while (line != null) {
+					// format:
+					// "Derzeitiges Limit: 4 Wünsche und 3 Grüße pro Hörer"
+					if (line.contains("Derzeitiges Limit:")) {
+						try {
+							allowedActions.limit = line.substring(
+									line.indexOf("Derzeitiges Limit:"),
+									line.indexOf("pro Hörer")).trim();
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+					if (line.contains("sind keine Grüße möglich.")) {
+						allowedActions.regards = false;
+					}
+					if (line.contains("die Playlist ist bereits voll")) {
+						allowedActions.wishes = false;
+					}
+					if (line.contains("sind nur in der moderierten Sendezeit möglich!")
+							|| line.contains("Aktuell On Air: MetalHead")) {
+						allowedActions.regards = false;
+						allowedActions.wishes = false;
+						allowedActions.moderated = false;
+						break;
+					}
+					line = httpResponse.readLine();
+				}
+				if (wishListener != null) {
+					wishListener.onWishesChecked(allowedActions);
+				}
 
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 
-                @Override
-                public void onTimeout() {
-                }
+		@Override
+		public void onTimeout() {
+		}
 
-                @Override
-                public void onError(String error) {
-                }
+		@Override
+		public void onError(String error) {
+		}
 
-                @Override
-                public void onCancel() {
-                }
+		@Override
+		public void onCancel() {
+		}
 
-            };
+	};
 
     public WishChecker(Context context, String URL) {
         grabber = new HTTPGrabber(context, URL, grabberListener);
     }
 
-    public void start() {
-        grabber.start();
-    }
+	public void start() {
+		grabber.start();
+	}
 
-    /**
-     * settings OnWishesCheckedListener
-     *
-     * @param listener OnWishesCheckedListener
-     */
-    public void setOnWishesCheckedListener(OnWishesCheckedListener listener) {
-        wishListener = listener;
-    }
+	/**
+	 * settings OnWishesCheckedListener
+	 * 
+	 * @param listener
+	 *            OnWishesCheckedListener
+	 */
+	public void setOnWishesCheckedListener(OnWishesCheckedListener listener) {
+		wishListener = listener;
+	}
 
 }
