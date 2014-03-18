cfa7af052f63684e3e9606b5835745c8a60ee7b7
Jan Lippert <ljan@mail.upb.de>
2014-03-18 19:27:12 +0100

    Added missing @EBean annotation

--- src/main/java/com/codingspezis/android/metalonly/player/utils/jsonapi/MyErrorHandler.java
+++ src/main/java/com/codingspezis/android/metalonly/player/utils/jsonapi/MyErrorHandler.java
@@ -1,8 +1,10 @@
 package com.codingspezis.android.metalonly.player.utils.jsonapi;
 
+import org.androidannotations.annotations.*;
 import org.androidannotations.api.rest.*;
 import org.springframework.web.client.*;
 
+@EBean
 public class MyErrorHandler implements RestErrorHandler {
     @Override
     public void onRestClientExceptionThrown(RestClientException e) {
