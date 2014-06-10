f1867c27cf54bb5cc2200edb4c87531850579031
Jan Lippert <ljan@mail.uni-paderborn.de>
2014-06-10 14:45:52 +0200

    Remoed unused imports

--- src/main/java/com/codingspezis/android/metalonly/player/stream/exceptions/WrongSampleRateException.java
+++ src/main/java/com/codingspezis/android/metalonly/player/stream/exceptions/WrongSampleRateException.java
@@ -1,13 +1,10 @@
 package com.codingspezis.android.metalonly.player.stream.exceptions;
 
-import com.spoledge.aacdecoder.*;
-
 /**
  * Thrown
  */
 @SuppressWarnings("serial")
 public class WrongSampleRateException extends PlayerException{
-
 
     public WrongSampleRateException(int rate) {
         super("Wrong sample rate detected: " + rate);
