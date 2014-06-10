c3ad69a65bbf22636bc5aafc38a88b0728f62471
Jan Lippert <ljan@mail.uni-paderborn.de>
2014-06-10 14:26:16 +0200

    Using new android gradle plugin and buildtools. fixed an error that was discovered via travis.

--- src/main/java/com/codingspezis/android/metalonly/player/stream/exceptions/WrongSampleRateException.java
+++ src/main/java/com/codingspezis/android/metalonly/player/stream/exceptions/WrongSampleRateException.java
@@ -6,15 +6,9 @@ package com.codingspezis.android.metalonly.player.stream.exceptions;
 @SuppressWarnings("serial")
 public class WrongSampleRateException extends Exception {
 
-    private int rate;
 
     public WrongSampleRateException(int rate) {
-        this.rate = rate;
-    }
-
-    @Override
-    public String getMessage() {
-        return "Wrong sample rate detected: " + rate;
+        super("Wrong sample rate detected: " + rate);
     }
 
 }
