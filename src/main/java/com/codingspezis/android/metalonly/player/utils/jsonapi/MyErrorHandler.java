cf3a54cfd15c16b9b0d22c4bbc62d0a064bae89d
Jan Lippert <lippertsjan@gmail.com>
2015-03-15 22:20:58 +0100

    MyErrorHandler implements correct method

--- src/main/java/com/codingspezis/android/metalonly/player/utils/jsonapi/MyErrorHandler.java
+++ src/main/java/com/codingspezis/android/metalonly/player/utils/jsonapi/MyErrorHandler.java
@@ -2,12 +2,18 @@ package com.codingspezis.android.metalonly.player.utils.jsonapi;
 
 import org.androidannotations.annotations.*;
 import org.androidannotations.api.rest.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.NestedRuntimeException;
 import org.springframework.web.client.*;
 
 @EBean
 public class MyErrorHandler implements RestErrorHandler {
-    @Override
-    public void onRestClientExceptionThrown(RestClientException e) {
 
+    public static final Logger LOGGER = LoggerFactory.getLogger(MyErrorHandler.class);
+
+    @Override
+    public void onRestClientExceptionThrown(NestedRuntimeException e) {
+        LOGGER.error(e.getMessage(), e);
     }
 }
