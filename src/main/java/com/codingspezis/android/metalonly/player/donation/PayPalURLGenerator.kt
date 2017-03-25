ac6e52343831bc88cc61bdfa292935afbcc04577
Jan Lippert <lippertsjan@gmail.com>
2017-03-25 12:39:03 +0200

    Simplified code

--- src/main/java/com/codingspezis/android/metalonly/player/donation/PayPalURLGenerator.kt
+++ src/main/java/com/codingspezis/android/metalonly/player/donation/PayPalURLGenerator.kt
@@ -1,5 +1,6 @@
 package com.codingspezis.android.metalonly.player.donation
 
+import java.io.UnsupportedEncodingException
 import java.net.URLEncoder
 
 object PayPalURLGenerator {
@@ -11,33 +12,22 @@ object PayPalURLGenerator {
      * @return PayPal URL for donating entered value
      */
     fun generatePaypalURL(donationValue: Float, donator: String): String {
-
-        // this should look like this:
-        // Vorname Nachname Spende METAL ONLY e.V.
-        // Anonym Spende METAL ONLY e.V.
-
-        val correctedDonator: String
-        if (donator.trim().length == 0) {
-            correctedDonator = "ANONYM"
-        } else {
-            correctedDonator = donator
-        }
-
-        var returnValue: String
         try {
-            val encodedDonationValue = URLEncoder.encode("" + donationValue, "UTF-8")
-            val encodedDonator = URLEncoder.encode(correctedDonator,
-                    "UTF-8")
+            val correctedDonator: String =
+                    if (donator.trim().isEmpty()) "ANONYM"
+                    else donator
 
-            returnValue = "https://www.paypal.com/cgi-bin/webscr?business=metalonly@gmx.de&cmd=_xclick&currency_code=EUR&amount=" +
-                    encodedDonationValue +
-                    "&item_name=" +
-                    encodedDonator +
-                    "%20Spende%20METAL%20ONLY%20e.V."
-        } catch (e: Exception) {
-            returnValue = "https://www.paypal.com/"
+            val encodedDonationValue = URLEncoder.encode("" + donationValue, "UTF-8")
+            val encodedDonator = URLEncoder.encode(correctedDonator, "UTF-8")
+
+            // TODO Should this be moved to @Rest?
+            "https://www.paypal.com/cgi-bin/webscr?" +
+                    "business=metalonly@gmx.de&cmd=_xclick&" +
+                    "currency_code=EUR&amount=$encodedDonationValue" +
+                    "&item_name=$encodedDonator%20Spende%20METAL%20ONLY%20e.V."
+        } catch (e: UnsupportedEncodingException) {
+            "https://www.paypal.com/"
         }
 
-        return returnValue
     }
 }
