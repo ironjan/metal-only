ddd99856d778f16af84264628198fdef4af3b2a6
Jan Lippert <lippertsjan@gmail.com>
2017-04-01 02:55:39 +0300

    Rewrote wish sending with okhttp

--- src/main/java/com/codingspezis/android/metalonly/player/fragments/WishFragment.java
+++ src/main/java/com/codingspezis/android/metalonly/player/fragments/WishFragment.java
@@ -30,14 +30,6 @@ import org.androidannotations.annotations.OptionsItem;
 import org.androidannotations.annotations.OptionsMenu;
 import org.androidannotations.annotations.UiThread;
 import org.androidannotations.annotations.ViewById;
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.protocol.HTTP;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
