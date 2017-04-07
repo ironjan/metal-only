bb103649cf62bf29e6f5399ff993a0e90f1defd6
Jan Lippert <lippertsjan@gmail.com>
2017-04-07 17:38:31 +0300

    CurrencyFormatInputFilter: java -> kt

---
+++ src/main/kotlin/com/codingspezis/android/metalonly/player/donation/CurrencyFormatInputFilter.kt
@@ -0,0 +1,31 @@
+package com.codingspezis.android.metalonly.player.donation
+
+import android.text.InputFilter
+import android.text.Spanned
+
+import java.util.regex.Matcher
+import java.util.regex.Pattern
+
+/**
+ * Filters EditText input so, that only correct values for money are accepted.
+ * http://stackoverflow.com/questions/7627148/edittext-with-currency-format
+ */
+class CurrencyFormatInputFilter : InputFilter {
+
+    internal var mPattern = Pattern.compile("(0|[1-9]+[0-9]*)?(\\.[0-9]{0,2})?")
+
+    override fun filter(source: CharSequence, start: Int, end: Int,
+                        dest: Spanned, dstart: Int, dend: Int): CharSequence? {
+
+        val result = dest.subSequence(0, dstart).toString() + source.toString()
+        dest.subSequence(dend, dest.length)
+
+        val matcher = mPattern.matcher(result)
+
+        if (!matcher.matches()) {
+            return dest.subSequence(dstart, dend)
+        }
+
+        return null
+    }
+}
