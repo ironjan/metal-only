1333d53012b0c7098c7348962152d81513bb765e
Jan Lippert <lippertsjan@gmail.com>
2017-04-18 00:58:27 +0300

    Removed unused import

--- src/main/java/com/codingspezis/android/metalonly/player/fragments/AboutFragment.java
+++ src/main/java/com/codingspezis/android/metalonly/player/fragments/AboutFragment.java
@@ -2,7 +2,6 @@ package com.codingspezis.android.metalonly.player.fragments;
 
 import android.content.Intent;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.widget.TextView;
