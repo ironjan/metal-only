80d085757f578723bb06204650b9d1df7e50c713
Jan Lippert <lippertsjan@gmail.com>
2017-04-27 19:14:11 +0300

    Configuring API URLs via config

--- src/main/java/com/codingspezis/android/metalonly/player/utils/UrlConstants.java
+++ src/main/java/com/codingspezis/android/metalonly/player/utils/UrlConstants.java
@@ -7,17 +7,11 @@ public final class UrlConstants {
     public static final String METAL_ONLY_DONATION_URL = "https://www.metal-only.de/?action=donation";
     public static final String YOUTUBE_SEARCH_URL = "https://www.youtube.com/results?search_query=";
 
-    public static final String METAL_ONLY_WUNSCHSCRIPT_POST_URL = "https://www.metal-only.de/?action=wunschscript&do=save";
-
     /**
      * Note that the Stream is the only non-https url!
      */
     public static final String STREAM_URL_128 = "http://server1.blitz-stream.de:4400";
 
-    public static final String METAL_ONLY_API_BASE_URL = "https://www.metal-only.de/botcon/mob.php?action=";
     public static final String API_OLD_PLAN_URL = "https://www.metal-only.de/botcon/mob.php?action=plan";
-    public static final String API_STATS_PATH = "stats";
-    public static final String API_PLAN_PATH = "plannew";
-    public static final String API_PLAN_WITH_STATS_PATH = "all";
     public static final String METAL_ONLY_MODERATOR_PIC_BASE_URL = "https://www.metal-only.de/botcon/mob.php?action=pic&nick=";
 }
