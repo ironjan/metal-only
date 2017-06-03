cfd91a210bf783c26af301cdda4e4d562f66aa0c
Jan Lippert <lippertsjan@gmail.com>
2017-06-03 12:15:41 +0300

    Using some androidannotations in PayPalDonationFragment now

--- src/main/kotlin/com/codingspezis/android/metalonly/player/donation/PayPalDonationFragment.kt
+++ src/main/kotlin/com/codingspezis/android/metalonly/player/donation/PayPalDonationFragment.kt
@@ -3,26 +3,33 @@ package com.codingspezis.android.metalonly.player.donation
 import android.content.Intent
 import android.content.SharedPreferences
 import android.net.Uri
-import android.os.Bundle
 import android.preference.PreferenceManager
 import android.support.v4.app.Fragment
 import android.text.InputFilter
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 
 import com.codingspezis.android.metalonly.player.R
+import org.androidannotations.annotations.AfterViews
+import org.androidannotations.annotations.EFragment
+import org.androidannotations.annotations.ViewById
+import org.androidannotations.annotations.res.StringRes
 
-class PayPalDonationFragment : Fragment() {
+@EFragment(R.layout.fragment_donation_paypal)
+open class PayPalDonationFragment: Fragment() {
 
-    private var editDonator: EditText? = null
-    private var editDonationValue: EditText? = null
+    @JvmField
+    @ViewById(R.id.editDonator)
+    protected var editDonator: EditText? = null
+    @JvmField
+    @ViewById(R.id.editDonationValue)
+    protected var editDonationValue: EditText? = null
 
-    private var btnSend: Button? = null
+    @JvmField
+    @ViewById(R.id.btnSend)
+    protected var btnSend: Button? = null
 
     private var prefs: SharedPreferences? = null
 
@@ -30,33 +37,17 @@ class PayPalDonationFragment : Fragment() {
 
     private var donationValue: Float = 0.toFloat()
 
-    private var actionLabel = "Weiter"
+    @JvmField
+    @StringRes(R.string.donation_toPaypal)
+    protected var actionLabel: String? = "Weiter"
 
-    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        val view = inflater!!.inflate(R.layout.fragment_donation_paypal,
-                container, false)
-
-        fetchResources()
-        findViews(view)
+    @AfterViews
+    fun afterViews() {
         bindActions()
         fetchPrefValues()
         bindPrefValues()
-
-        return view
     }
 
-    private fun fetchResources() {
-        actionLabel = resources.getString(R.string.donation_toPaypal)
-    }
-
-    private fun findViews(view: View) {
-        editDonator = view.findViewById(R.id.editDonator) as EditText
-        editDonationValue = view
-                .findViewById(R.id.editDonationValue) as EditText
-        btnSend = view.findViewById(R.id.btnSend) as Button
-
-    }
 
     private fun bindActions() {
         editDonationValue!!.filters = arrayOf<InputFilter>(CurrencyFormatInputFilter())
@@ -68,8 +59,7 @@ class PayPalDonationFragment : Fragment() {
         editDonationValue!!.setImeActionLabel(actionLabel,
                 EditorInfo.IME_ACTION_GO)
 
-        editDonationValue!!
-                .setOnEditorActionListener { v, actionId, event ->
+        editDonationValue!!.setOnEditorActionListener { _, actionId, _ ->
                     when (actionId) {
                         EditorInfo.IME_ACTION_NONE,
                             // setting action does nothing..
@@ -84,28 +74,23 @@ class PayPalDonationFragment : Fragment() {
     }
 
     private fun fetchPrefValues() {
-        prefs = PreferenceManager
-                .getDefaultSharedPreferences(activity)
+        prefs = PreferenceManager.getDefaultSharedPreferences(activity)
         donator = prefs!!.getString(getString(R.string.paypal_key_sender), "")
         try {
-            donationValue = prefs!!.getFloat(
-                    getString(R.string.paypal_key_value), -1.0f)
+            donationValue = prefs!!.getFloat(getString(R.string.paypal_key_value), -1.0f)
         } catch (e: ClassCastException) {
             fetchDeprecatedDonationValuePref()
         }
-
     }
 
     private fun fetchDeprecatedDonationValuePref() {
         // Because this pref was a String in earlier versions
         try {
-            val donationValueDeprecated = prefs!!.getString(
-                    getString(R.string.paypal_key_value), "-1.0")
+            val donationValueDeprecated = prefs!!.getString(getString(R.string.paypal_key_value), "-1.0")
             donationValue = java.lang.Float.valueOf(donationValueDeprecated)!!
         } catch (ex: NumberFormatException) {
             donationValue = -1.0f
         }
-
     }
 
     private fun bindPrefValues() {
@@ -119,14 +104,12 @@ class PayPalDonationFragment : Fragment() {
         updateValues()
 
         if (donationValue <= 0) {
-            Toast.makeText(activity,
-                    "Der Spendenbetrag kann nicht leer sein.",
+            Toast.makeText(activity, "Der Spendenbetrag kann nicht leer sein.",
                     Toast.LENGTH_LONG).show()
             return
         }
 
-        val paypalURL = PayPalURLGenerator.generatePaypalURL(
-                donationValue, donator!!)
+        val paypalURL = PayPalURLGenerator.generatePaypalURL(donationValue, donator!!)
         val paypalUri = Uri.parse(paypalURL)
         val paypalIntent = Intent(Intent.ACTION_VIEW, paypalUri)
         startActivity(paypalIntent)
@@ -136,12 +119,10 @@ class PayPalDonationFragment : Fragment() {
         donator = editDonator!!.text.toString()
 
         try {
-            donationValue = java.lang.Float.parseFloat(editDonationValue!!.text
-                    .toString())
+            donationValue = java.lang.Float.parseFloat(editDonationValue!!.text.toString())
         } catch (e: NumberFormatException) {
             donationValue = -1.0f
         }
-
     }
 
     override fun onPause() {
