911d5d57d72681633113ab3d70bc5a73da613168
Jan Lippert <lippertsjan@gmail.com>
2017-06-16 18:47:58 +0200

    Removed deprecated constructor and simplified Song class

--- core/src/main/kotlin/com/codingspezis/android/metalonly/player/core/Song.kt
+++ core/src/main/kotlin/com/codingspezis/android/metalonly/player/core/Song.kt
@@ -1,45 +1,10 @@
 package com.codingspezis.android.metalonly.player.core
 
-import java.util.Calendar
-
 /**
  * class for holding a song
  */
-public class Song {
-    val interpret: String
-    val title: String
-    val date: Long
-
-    val thumb: String
-
-    /**
-     * Creates a Song with the given parameters
-     * @param interpret song artist
-     * @param title song title
-     * @param thumb name of getModerator's picture
-     * @param date when this song was played
-     */
-    constructor(interpret: String, title: String, thumb: String, date: Long) : super() {
-        this.interpret = interpret
-        this.title = title
-        this.thumb = thumb.replace(" OnAir", "")
-        this.date = date
-    }
-
-    /**
-     * Wrapper for [.Song] with date set to the moment this constructor is called
-     * @param interpret song artist
-     * @param title song title
-     * @param thumb name of getModerator's picture
-     *
-     * @deprecated Use the full constructor instead
-     */
-    @JvmOverloads constructor(interpret: String, title: String, thumb: String = "") : super() {
-        this.interpret = interpret
-        this.title = title
-        this.thumb = thumb.replace(" OnAir", "")
-        this.date = Calendar.getInstance().timeInMillis
-    }
+class Song(val interpret: String, val title: String, thumb: String, val date: Long) {
+    val thumb: String = thumb.replace(" OnAir", "")
 
     fun withClearedThumb(): Song {
         return Song(interpret, title, "", date)
@@ -50,4 +15,5 @@ public class Song {
      */
     val isValid: Boolean
         get() = interpret.isNotEmpty() && title.isNotEmpty()
+
 }
