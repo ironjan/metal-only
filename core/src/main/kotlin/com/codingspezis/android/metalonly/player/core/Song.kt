d96b300c761cf576536d40850d1662a6f06d2991
Jan Lippert <lippertsjan@gmail.com>
2017-06-19 13:26:20 +0200

    Song now implements the necessary interfaces

--- core/src/main/kotlin/com/codingspezis/android/metalonly/player/core/Song.kt
+++ core/src/main/kotlin/com/codingspezis/android/metalonly/player/core/Song.kt
@@ -1,16 +1,25 @@
 package com.codingspezis.android.metalonly.player.core
 
+import java.util.Date
+
 /**
  * Represents a song with its properties. <code>thumb</code> is a cleaned variant of <code>thumbRaw</code>,
  * i.e. <code>thumb</code> without <code>" OnAir"</code>.
+ *
+ * TODO Clean up the overrides and remove dupolicate fields
  */
-data class Song(val interpret: String, val title: String, val thumbRaw: String, val date: Long) {
+data class Song(val interpret: String, override val title: String, val thumbRaw: String, val date: Long)
+    : Track, HistoricSongExtension {
     val thumb: String = thumbRaw.replace(" OnAir", "")
-
     fun withClearedThumb(): Song {
         return Song(interpret, title, "", date)
     }
-
     val isValid: Boolean
         get() = interpret.isNotEmpty() && title.isNotEmpty()
+
+    override val moderator: String = thumb
+
+    override val artist: String = interpret
+
+    override val datePlayed: Date = Date(date)
 }
