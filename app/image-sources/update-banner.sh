9efcb73c3035305bfb2412955761b2e17cad412a
Jan Lippert <lippertsjan@gmail.com>
2017-07-19 23:03:23 +0200

    Switched to toolbar and added logo on top

---
+++ app/image-sources/update-banner.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+convert mo_banner.png -resize x192 ../src/main/res/drawable-xxxhdpi/mo_banner.png 
+convert mo_banner.png -resize x144 ../src/main/res/drawable-xxhdpi/mo_banner.png 
+convert mo_banner.png -resize x96 ../src/main/res/drawable-xhdpi/mo_banner.png 
+convert mo_banner.png -resize x72 ../src/main/res/drawable-hdpi/mo_banner.png
