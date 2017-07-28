be48d4b43932d371fcc20565e4c7c0579c8daaff
Jan Lippert <lippertsjan@gmail.com>
2017-07-28 17:05:25 +0200

    Improved open pr script

--- scripts/open-pr.sh
+++ scripts/open-pr.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-BRANCH=$1
-nohup chromium "https://github.com/ironjan/metal-only/compare/$BRANCH?expand=1" &
+CURRENT_BRANCH=$(git branch | grep \* | cut -d ' ' -f2)
+BASE_BRANCH=$1
+if [ -z "$BASE_BRANCH" ]; then
+  echo "Set target branch to beta"
+  BASE_BRANCH=beta
+fi
+
+nohup chromium "https://github.com/ironjan/metal-only/compare/$BASE_BRANCH...$CURRENT_BRANCH" &
