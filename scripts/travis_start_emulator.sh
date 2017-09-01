10308b2b5847a77556b789c2927e419cf0acc74f
Jan Lippert <lippertsjan@gmail.com>
2017-09-01 22:34:37 +0200

    Fixed build

--- scripts/travis_start_emulator.sh
+++ scripts/travis_start_emulator.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo no | android create avd --force -n test -t android-16 --abi armeabi-v7a
+echo no | android create avd --force -n test -t android-19 --abi armeabi-v7a
 emulator -avd test -no-audio -no-window &
 android-wait-for-emulator
 sleep 180
