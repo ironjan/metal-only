a61cd946edfade8ced0b054bec84f4eb8f5e673e
Jan Lippert <lippertsjan@gmail.com>
2018-02-18 14:36:21 +0100

    Sending log file

--- app/src/main/kotlin/com/codingspezis/android/metalonly/player/utils/FeedbackMailer.kt
+++ app/src/main/kotlin/com/codingspezis/android/metalonly/player/utils/FeedbackMailer.kt
@@ -7,9 +7,12 @@ import android.os.Build
 import android.widget.Toast
 import com.codingspezis.android.metalonly.player.BuildConfig
 import com.codingspezis.android.metalonly.player.R
+import com.hypertrack.hyperlog.HyperLog
 import org.androidannotations.annotations.EBean
 import org.androidannotations.annotations.RootContext
 import org.androidannotations.annotations.res.StringRes
+import java.io.File
+
 
 @EBean
 open class FeedbackMailer {
@@ -28,15 +31,27 @@ open class FeedbackMailer {
     fun sendEmail() {
         val subject = "[$app_name ${BuildConfig.VERSION_NAME}] Feedback, Fehler"
 
+        val logfile: File? = HyperLog.getDeviceLogsInFile(context)
+        val logFileUri = Uri.fromFile(logfile)
+
         val body = "\n\n\n---\n" +
-                "$app_name\nVersion: ${BuildConfig.VERSION_NAME}\n" +
-                "Android: ${Build.VERSION.RELEASE}\n" +
-                "Model: ${Build.MODEL}"
+                    "$app_name\nVersion: ${BuildConfig.VERSION_NAME}\n" +
+                    "Android: ${Build.VERSION.RELEASE}\n" +
+                    "Model: ${Build.MODEL}"
+
+
+
 
         val emailIntent = Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:"))
         emailIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(mailaddress_codingspezis))
         emailIntent.putExtra(Intent.EXTRA_SUBJECT, subject)
         emailIntent.putExtra(Intent.EXTRA_TEXT, body)
+        HyperLog.e("FeedbackMailer", "Sending mail")
+        if(logfile != null) {
+            HyperLog.e("FeedbackMailer", "Sending mail with attachement")
+            emailIntent.putExtra(Intent.EXTRA_STREAM, logFileUri)
+        }
+
         try {
             context.startActivity(Intent.createChooser(emailIntent, mailaddress_codingspezis))
         } catch (ex: android.content.ActivityNotFoundException) {
