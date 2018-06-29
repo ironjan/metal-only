ce1b5e7f1ccd12f352b47910cd28155f3437baa5
Jan Lippert <lippertsjan@gmail.com>
2018-06-29 13:38:48 +0200

    Removed cipher suites from new test

--- app/src/main/java/com/codingspezis/android/metalonly/player/TemporaryTestActivity.java
+++ app/src/main/java/com/codingspezis/android/metalonly/player/TemporaryTestActivity.java
@@ -226,11 +226,6 @@ public class TemporaryTestActivity extends AppCompatActivity {
             statusUpdate("Test " + testName + ": Started");
             ConnectionSpec spec = new ConnectionSpec.Builder(ConnectionSpec.MODERN_TLS)
                     .tlsVersions(TlsVersion.TLS_1_2)
-                    .cipherSuites(
-                            CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-                            CipherSuite.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-                            CipherSuite.TLS_DHE_RSA_WITH_AES_128_GCM_SHA256,
-                            CipherSuite.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384)
                     .build();
 
             OkHttpClient client = new OkHttpClient.Builder()
