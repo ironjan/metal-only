206d832c140afd52fd4095a95328a08b9bfda92f
Jan Lippert <lippertsjan@gmail.com>
2019-07-10 17:44:46 +0200

    Did some more work

--- app/src/main/java/com/codingspezis/android/metalonly/player/MainActivity.kt
+++ app/src/main/java/com/codingspezis/android/metalonly/player/MainActivity.kt
@@ -2,12 +2,18 @@ package com.codingspezis.android.metalonly.player
 
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.support.v4.content.res.ResourcesCompat
 import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var textMessage: TextView
+    private lateinit var action_play_stop: MenuItem
+
+    var isPlaying: Boolean = false
+
     private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_wish -> {
@@ -15,7 +21,13 @@ class MainActivity : AppCompatActivity() {
                 return@OnNavigationItemSelectedListener true
             }
             R.id.action_play_stop -> {
-                textMessage.setText(R.string.title_play)
+                if(isPlaying) {
+                    textMessage.setText("Stopped")
+                    action_play_stop.icon = ResourcesCompat.getDrawable(resources, R.drawable.ic_action_play, theme)
+                }else{
+                    textMessage.setText("Playing")
+                    action_play_stop.icon = ResourcesCompat.getDrawable(resources, R.drawable.ic_action_stop, theme)
+                }
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_plan -> {
@@ -33,5 +45,7 @@ class MainActivity : AppCompatActivity() {
 
         textMessage = findViewById(R.id.message)
         navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+
+        action_play_stop = navView.menu.findItem(R.id.action_play_stop)
     }
 }
