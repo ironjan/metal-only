469568fedaab3aaa15f1c4b317aae497cf218f3c
Jan Lippert <lippertsjan@gmail.com>
2019-07-28 21:41:14 +0200

    Started a new rewrite

---
+++ rewrite/app/src/main/java/de/ironjan/metalonly/ui/main/SectionsPagerAdapter.kt
@@ -0,0 +1,34 @@
+package de.ironjan.metalonly.ui.main
+
+import android.content.Context
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import de.ironjan.metalonly.R
+
+private val TAB_TITLES = arrayOf(
+    R.string.tab_text_1,
+    R.string.tab_text_2
+)
+
+/**
+ * A [FragmentPagerAdapter] that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+class SectionsPagerAdapter(private val context: Context, fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+    override fun getItem(position: Int): Fragment {
+        // getItem is called to instantiate the fragment for the given page.
+        // Return a PlaceholderFragment (defined as a static inner class below).
+        return PlaceholderFragment.newInstance(position + 1)
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return context.resources.getString(TAB_TITLES[position])
+    }
+
+    override fun getCount(): Int {
+        // Show 2 total pages.
+        return 2
+    }
+}
