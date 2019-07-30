ee0283f40d4044660a3a4c9331f83ca7bf568536
Jan Lippert <lippertsjan@gmail.com>
2019-07-30 22:32:08 +0200

    Added missing API calls

--- rewrite/app/src/main/java/de/ironjan/metalonly/api/Client.kt
+++ rewrite/app/src/main/java/de/ironjan/metalonly/api/Client.kt
@@ -1,31 +1,73 @@
 package de.ironjan.metalonly.api
 
 import android.content.Context
+import arrow.core.Either
 import com.google.gson.reflect.TypeToken
 import com.koushikdutta.ion.Ion
+import com.koushikdutta.ion.builder.Builders
 import de.ironjan.metalonly.api.model.Stats
+import java.io.PrintWriter
+import java.io.StringWriter
+import java.io.Writer
 import java.util.concurrent.TimeUnit
 
-// FIXME add error handling
-class Client (val context: Context) {
-    private val baseUrl = "http://mensaupb.herokuapp.com/metalonly"
+class Client(private val context: Context) {
 
-    private val statsPath = "/stats"
+    fun getStats(noCache: Boolean) = safeRequest(statsUrl, noCache)
 
-    fun getStats(): Stats? {
-        return Ion.with(context)
-            .load("$baseUrl$statsPath")
-            .`as`(object : TypeToken<Stats>() {})
-            .get(5, TimeUnit.SECONDS)
+    fun getShowInfo(noCache: Boolean) = safeRequest(showInfoUrl, noCache)
+
+    fun getMods(noCache: Boolean) = safeRequest(modsUrl, noCache)
+
+    fun getPlan(noCache: Boolean) = safeRequest(planUrl, noCache)
+
+    fun getTrack(noCache: Boolean) = safeRequest(trackUrl, noCache)
+
+    private fun safeRequest(url: String, noCache: Boolean): Either<String, Stats> =
+        try {
+            Either.right(tryExecute(prepareRequest(url, noCache)))
+        } catch (e: Exception) {
+            wrapException(e)
+        }
+
+    private fun <T> tryExecute(preparedRequest: Builders.Any.B): T =
+        preparedRequest
+            .`as`(object : TypeToken<T>() {})
+            .get(REQUEST_TIMEOUT_30_SECONDS, TimeUnit.SECONDS)
+
+    private fun wrapException(e: Exception): Either<String, Nothing> {
+        val sw: Writer = StringWriter()
+        e.printStackTrace(PrintWriter(sw))
+        return Either.left(sw.toString())
     }
 
-    /* TODO
-GET /stats            metalonly.controllers.StatsController.stats()
-GET /showinformation  metalonly.controllers.StatsController.showinformation()
-GET /track            metalonly.controllers.StatsController.track()
-GET /plan            metalonly.controllers.PlanController.plan()
-GET /mods            metalonly.controllers.ModController.getMods()
 
-POST /wish           metalonly.controllers.WishController.submit()
-     */
+    private fun prepareRequest(url: String, forceReload: Boolean): Builders.Any.B {
+        val requestBuilder = Ion.with(context).load(url)
+//                .setLogging("ClientV3", Log.DEBUG)
+        return if (forceReload) {
+            requestBuilder.noCache()
+        } else {
+            requestBuilder
+        }
+    }
+
+
+    companion object {
+        const val REQUEST_TIMEOUT_30_SECONDS = 30000L
+        private const val baseUrl = "https://mensaupb.herokuapp.com/api"
+
+        private const val statsPath = "/stats"
+
+        private const val trackPath = "/track"
+        private const val showInformationPath = "/showinformation"
+        private const val planPath = "/plan"
+        private const val modsPath = "/mods"
+
+        const val statsUrl = "$baseUrl$statsPath"
+        const val showInfoUrl = "$baseUrl$showInformationPath"
+        const val trackUrl = "$baseUrl$trackPath"
+        const val planUrl = "$baseUrl$planPath"
+        const val modsUrl = "$baseUrl$modsPath"
+    }
 }
