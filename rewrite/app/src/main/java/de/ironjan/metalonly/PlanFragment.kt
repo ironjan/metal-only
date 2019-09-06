d16a8e4eda4aae776b71753dd5f805a6103d6032
Jan Lippert <lippertsjan@gmail.com>
2019-09-06 13:44:09 +0200

    Added fake data for testing

--- rewrite/app/src/main/java/de/ironjan/metalonly/PlanFragment.kt
+++ rewrite/app/src/main/java/de/ironjan/metalonly/PlanFragment.kt
@@ -9,6 +9,7 @@ import android.widget.Toast
 import arrow.core.right
 import de.ironjan.metalonly.api.Client
 import de.ironjan.metalonly.api.model.PlanEntry
+import de.ironjan.metalonly.api.model.ShowInfo
 import de.ironjan.metalonly.log.LW
 import kotlinx.android.synthetic.main.fragment_plan.*
 
@@ -36,6 +37,8 @@ class PlanFragment : Fragment() {
     }
 
     private fun refresh() {
+        fakeRefresh()
+
 
         val lContext = context ?: return
         val either = Client(lContext).getPlan()
@@ -50,6 +53,46 @@ class PlanFragment : Fragment() {
             LW.d(TAG, "Loading done")
             either.map { showPlan(it) }
         }
+    }
+
+    private fun fakeRefresh() {
+        val mHead = ShowInfo("MetalHead", "Keine Grüsse und Wünsche möglich.","Mixed Metal")
+
+        val plan = listOf(
+                PlanEntry("2019-09-02T00:00","2019-09-02T15:00", mHead),
+                PlanEntry("2019-09-02T15:00", "2019-09-02T18:00",  ShowInfo("Wolle", "Slow Motion", "Stoner-, Doom, Grunge und Souther- Psychedelic Rock & Metal")),
+                PlanEntry("2019-09-02T18:00", "2019-09-02T21:00",  ShowInfo("Mick", "New Limits", "NEW Mixed Rock & Metal")),
+                PlanEntry("2019-09-02T21:00", "2019-09-02T23:00",  ShowInfo("MaRs", "MaRs' Metalmix", "Mixed Rock & Metal")),
+                PlanEntry("2019-09-02T23:00", "2019-09-03T15:00",  mHead),
+                PlanEntry("2019-09-03T15:00", "2019-09-03T18:00",  ShowInfo("Blacky", "Metal Mayhem", "Mixed Rock & Metal")),
+                PlanEntry("2019-09-03T18:00", "2019-09-03T21:00",  ShowInfo("Vincent", "Krachen im Gebälk", "Death, Thrash, Pagan, Folk Metal")),
+                PlanEntry("2019-09-03T21:00", "2019-09-03T23:00",  ShowInfo("Slaine", "METALFORCE WARP SPASM", "Mixed Metal")),
+                PlanEntry("2019-09-03T23:00", "2019-09-04T15:00",  mHead),
+                PlanEntry("2019-09-04T15:00", "2019-09-04T18:00",  ShowInfo("Slaine", "METALFORCE", "Mixed Rock & Metal")),
+                PlanEntry("2019-09-04T18:00", "2019-09-04T20:00",  ShowInfo("Wolle", "Satan was a Biker!", "Hard Rock, Heavy-, Speed-, Thrash Metal")),
+                PlanEntry("2019-09-04T20:00", "2019-09-04T23:00",  ShowInfo("MaRs", "Loud & Heavy", "Death & Blackmetal")),
+                PlanEntry("2019-09-04T23:00", "2019-09-05T15:00",  mHead),
+                PlanEntry("2019-09-05T15:00", "2019-09-05T17:00",  mHead),
+                PlanEntry("2019-09-05T17:00", "2019-09-05T20:00",  ShowInfo("Wolle", "Hoch die Hörner", "Folk-, Mittelalter-, Pagan- & Viking Metal")),
+                PlanEntry("2019-09-05T20:00", "2019-09-05T23:00",  ShowInfo("MaRs", "MaRs' Metalmix", "Mixed Rock & Metal")),
+                PlanEntry("2019-09-05T23:00", "2019-09-06T14:00",  mHead),
+                PlanEntry("2019-09-06T14:00", "2019-09-06T16:00",  ShowInfo("Slaine", "HEARTLANDS", "Folk und Mittelalter Rock und Metal")),
+                PlanEntry("2019-09-06T16:00", "2019-09-06T18:00",  ShowInfo("Blacky", "Metal Mayhem - Dark Edition", "Black-, Death-, Doom-, Pagan & Viking Metal")),
+                PlanEntry("2019-09-06T18:00", "2019-09-06T20:00",  ShowInfo("Wolle", "Metal Weekeeeend", "Mixed Rock &  Metal")),
+                PlanEntry("2019-09-06T20:00", "2019-09-06T23:00",  ShowInfo("Yoda", "Yodas Thrash Attack", "Speed, Thrash Metal")),
+                PlanEntry("2019-09-06T23:00", "2019-09-07T14:00",  mHead),
+                PlanEntry("2019-09-07T14:00", "2019-09-07T17:00",  ShowInfo("Wolle", "Einmal Musik bitte, mit allem!", "Mixed Rock & Metal")),
+                PlanEntry("2019-09-07T17:00", "2019-09-07T19:00",  ShowInfo("frei", "", "")),
+                PlanEntry("2019-09-07T19:00", "2019-09-07T23:00",  ShowInfo("Slaine", "Metalforce", "Mixed Rock & Metal")),
+                PlanEntry("2019-09-07T23:00", "2019-09-08T14:00",  mHead),
+                PlanEntry("2019-09-08T14:00", "2019-09-08T18:00",  ShowInfo("Godshand", "Godis 2nd Hand Musikladen", "Mixed Rock & Metal")),
+                PlanEntry("2019-09-08T18:00", "2019-09-08T20:00",  ShowInfo("frei", "", "")),
+                PlanEntry("2019-09-08T20:00", "2019-09-08T23:00",  ShowInfo("Wolle", "Oldies und Klassiker der Rockgeschichte!", "60er bis Mitte 90er Rock & Metal")),
+                PlanEntry("2019-09-08T23:00", "2019-09-09T00:00",  mHead))
+
+
+        showPlan(plan)
+
     }
 
     private fun showPlan(plan: List<PlanEntry>) {
